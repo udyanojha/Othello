668230c45d34a276731d8c51f10b1fa56a6bb13f
udyanojha <use 51847297+udyanojha@users.noreply.github.com>
2022-08-16 23:37:12 +0530

    Player Class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,20 @@
 public class Player {
-
+    private String name;
+    private int piece;
+    public Player(String name, int num) {
+        setName(name);
+        setPiece(num);
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setPiece(int piece) {
+        this.piece = piece;
+    }
+    public int getPiece() {
+        return this.piece;
+    }
 }
